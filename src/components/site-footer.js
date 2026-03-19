6bcfb5c62fd14fc6b0db79fcfcc3a782c8c4c5d0
celestiawana <wyang73@my.bcit.ca>
2026-03-20 06:03:37 +0800

    Restructure image files: move all images to public folder

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -2,21 +2,21 @@ class SiteFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
         <footer class="fixed-bottom footer">
-            <ul class="footer-nav">
+            <ul class="footer-nav d-flex justify-content-center">
             <li onclick="window.location.href='main.html'">
-                <a href="#"><span class="material-symbols-outlined">home</span>
+                <a href="#" class="text-dark d-flex flex-column align-items-center"><span class="material-symbols-outlined">home</span>
                 <span class="home">home</span></a>
             </li>
-            <li>
-                <a href="#"><span class="material-symbols-outlined">history_2</span>
+            <li onclick="window.location.href='recent .html'">
+                <a href="#" class="text-dark d-flex flex-column align-items-center"><span class="material-symbols-outlined">history_2</span>
                 <span class="Recent">Recent</span></a>
             </li>
-            <li>
-                <a href="#"><span class="material-symbols-outlined">calendar_meal</span>
+            <li onclick="window.location.href='restaurant.html'">
+                <a href="#" class="text-dark d-flex flex-column align-items-center"><span class="material-symbols-outlined">calendar_meal</span>
                 <span class="Reservation">Reservation</span></a>
             </li>
             <li onclick="window.location.href='favorites.html'">
-                <a href="#"><span class="material-symbols-outlined">favorite</span>
+                <a href="#"class="text-dark d-flex flex-column align-items-center"><span class="material-symbols-outlined">favorite</span>
                 <span class="favorite">Favorite</span></a>
             </li>
             </ul>
--- src/components/site-navbar.js
+++ src/components/site-navbar.js
@@ -7,10 +7,10 @@ class SiteNavbar extends HTMLElement {
   }
 
   renderNavbar() {
-    onAuthReady(user => {
-        const href = user ? "./main.html" : "./index.html";
+    onAuthReady((user) => {
+      const href = user ? "./main.html" : "./index.html";
 
-        this.innerHTML = `
+      this.innerHTML = `
             <nav class="navbar navbar-expand-lg">
                 <div class="container d-flex justify-content-start ms-0 ps-4 gap-2">
                 <div class="dropdown">
@@ -24,11 +24,21 @@ class SiteNavbar extends HTMLElement {
                     </ul>
                 </div>
                 <a class="navbar-brand col-md-auto" href="${href}">
-                    Website Name
+                    TimesUp
                 </a>
                 </div>
+                      <div class="d-flex gap-2">
+        <input
+          type="text"
+          class="address-input form-control py-3"
+          placeholder="Enter address"
+        />
+        <button class="search-button btn btn-danger">Search</button>
+        <button class="open-map-button btn btn-success">Map</button>
+      </div>
+                <form class="d-flex">
                 <div class="d-flex flex-row gap-3 pe-5">
-                <button id="loginBtn" class="login col-md-auto rounded-pill" onclick="window.location.href='login.html'"
+                <button id="loginBtn" class="login 3 rounded-pill" onclick="window.location.href='login.html'"
                     type="button">Log in</button>
 
                 <button id="logOutBtn" class="login col-md-auto rounded-pill me-4" type="button">Log Out</button>
@@ -36,8 +46,10 @@ class SiteNavbar extends HTMLElement {
             </nav>
         `;
 
-        this.querySelector('#logOutBtn').addEventListener('click', () => logoutUser());
-    })
+      this.querySelector("#logOutBtn").addEventListener("click", () =>
+        logoutUser(),
+      );
+    });
   }
 }
 
